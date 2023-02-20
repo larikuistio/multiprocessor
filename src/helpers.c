85fab97dadac0988ef0abadb543248580c7ecf1a
Lari <lari@kuistio.me>
2023-02-20 15:09:52 +0200

    added encodeImage

--- src/helpers.c
+++ src/helpers.c
@@ -15,3 +15,18 @@ unsigned char* decodeImage(const char* filename, unsigned char* image, unsigned*
 
 	return image;
 }
+
+void encodeImage(const char* filename, unsigned char* image, unsigned* width, unsigned* height) {
+
+	unsigned error;
+	unsigned char* png = 0;
+	size_t pngsize;
+
+	error = lodepng_encode32(&png, &pngsize, image, *width, *height);
+	if(!error) lodepng_save_file(png, pngsize, filename);
+	if(error) printf("error %u: %s\n", error, lodepng_error_text(error));
+
+	free(png);
+
+}
+
