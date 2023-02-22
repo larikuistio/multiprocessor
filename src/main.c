5e6014b7d66fa8bc769afb8d7e4a7bc3d52b98fc
Lari <lari@kuistio.me>
2023-02-22 16:51:00 +0200

    added converttograyscale

--- src/main.c
+++ src/main.c
@@ -15,13 +15,14 @@ int main(void) {
     //const char* filename2 = "images/test1.png";
     const char* filename3 = "images/resized.png";
     unsigned char* image3 = 0;
+    unsigned char* image4 = 0;
     unsigned newwidth, newheight;
     resizeImage(image, &image3, &width, &height, &newwidth, &newheight);
-
+    convertToGrayscale(image3, &image4, &newwidth, &newheight, 4);
     const char* filename1 = "images/test.png";
     encodeImage(filename1, image, &width, &height);
     // encodeImage(filename2, image2, &width, &height);
-    encodeImage(filename3, image3, &newwidth, &newheight);
+    encodeImage(filename3, image4, &newwidth, &newheight);
     printf("Program finished\n");
     free(image);
     // free(image2);
