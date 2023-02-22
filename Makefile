1bda51563e11e7d5763f1a8f11a563eacec3b7d4
Vilatsut <ville.hokkinen@gmail.com>
2023-02-22 15:48:14 +0200

    Cleaning

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ main: src/main.c src/helpers.c src/lodepng.c
 	gcc -I ./inc src/main.c src/helpers.c src/lodepng.c -o main $(CFLAGS)
 
 clean:
-	rm -f main
+	rm -f main debug
 
 debug: src/main.c src/helpers.c src/lodepng.c 
 	clang -I inc src/main.c src/helpers.c src/lodepng.c -lOpenCL -o debug -D_FORTIFY_SOURCE=2  -g3 -O0 -Wall -Wextra -fsanitize=leak -fsanitize=address $(CFLAGS)
--- src/main.c
+++ src/main.c
@@ -6,22 +6,11 @@
 #include <inttypes.h>
 
 int main(void) {
-    const char* filename = "images/im0.png";
+    const char* filename = "images/im1.png";
     unsigned char* image = 0;
     unsigned width, height;
     decodeImage(filename, &image, &width, &height);
-    
-    /*unsigned char* image2 = malloc(2016 * 2940 * 4 * sizeof(unsigned char));
-    if (image2 == NULL) {
-        printf("malloc failed\n");
-        return EXIT_FAILURE;
-    }
-    unsigned char* image1 = image;
-    for(int i = 0; i < 23708160; i++)
-    {
-        image2[i] = *image1;
-        image1++;
-    }*/
+
 
     const char* filename1 = "images/test.png";
     //const char* filename2 = "images/test1.png";
@@ -29,13 +18,15 @@ int main(void) {
     unsigned char* image3 = 0;
     unsigned newwidth, newheight;
     resizeImage(image, &image3, &width, &height, &newwidth, &newheight);
+
+    const char* filename1 = "images/test.png";
     encodeImage(filename1, image, &width, &height);
-    //encodeImage(filename2, image2, &width, &height);
+    // encodeImage(filename2, image2, &width, &height);
     encodeImage(filename3, image3, &newwidth, &newheight);
     printf("Program finished\n");
     free(image);
-    //free(image1);
-    //free(image2);
+    // free(image2);
     free(image3);
+
     return EXIT_SUCCESS;
 }
