c6f4a5eedd1b27872447fa770935ae1a61960685
Lari <lari@kuistio.me>
2023-03-31 13:50:48 +0300

    zncc

--- src/phase2.c
+++ src/phase2.c
@@ -6,24 +6,57 @@
 #define MAX_DISPARITY 64
 #define MIN_DISPARITY 0
 #define THRESHOLD 2
+#define B 9
 
-int calcZNCC() {
-
-}
-
-
-int crossCheck(unsigned char left, unsigned char image, unsigned short threshold, unsigned short width, unsigned short height) {
-  
-  for(size_t j = 0; j < height; j++) {
-    for(size_t i = 0; i < width; i++) {
-      for(size_t d = MA_DISPARI)
+unsigned char calcZNCC(unsigned char *left, unsigned char *right, unsigned short width, unsigned short height)
+{
+    for (size_t j = 0; j < height; j++)
+    {
+        for (size_t i = 0; i < width; i++)
+        {
+            for (size_t d = 0; d < MAX_DISPARITY; d++)
+            {
+                unsigned int window_avg_l = 0;
+                unsigned int window_avg_r = 0;
+                for(int y = -(B-1)/2; y < (B-1)/2; y++)
+                {
+                    for(int x = -(B-1)/2; x < (B-1)/2; x++)
+                    {
+                        window_avg_l += left[(j + y) * width + (i + x)];
+                        window_avg_l += left[(j + y) * width + (i + x)];
+                    }
+                }
+                window_avg_l = B*B;
+                window_avg_r = B*B;
+            }
+        }
     }
-  }
 }
 
+int crossCheck(unsigned char *left, unsigned char *image, unsigned short width, unsigned short height)
+{
 
-int occlusionFill(void) {
+    for (size_t j = 0; j < height; j++)
+    {
+        for (size_t i = 0; i < width; i++)
+        {
+            for (size_t d = 0; d < MAX_DISPARITY; d++)
+            {
+                unsigned int sum = 0;
+                for(int y = B/2; y < B/2; y++)
+                {
+                    for(int x = B/2; x < B/2; x++)
+                    {
+                        sum += 
+                    }
+                }
+            }
+        }
+    }
+}
 
+int occlusionFill(void)
+{
 }
 
 int normalize(unsigned char* disparity_img, unsigned width, unsigned height) {
@@ -44,31 +77,32 @@ int normalize(unsigned char* disparity_img, unsigned width, unsigned height) {
 	}
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 
-    if (argc < 2) {
-      printf("provide input and output image files names as arguments\n");
-      return EXIT_FAILURE;
+    if (argc < 2)
+    {
+        printf("provide input and output image files names as arguments\n");
+        return EXIT_FAILURE;
     }
 
-    const char* inputimg_r = argv[1];
-    const char* inputimg_l = argv[2];
-    const char* outputimg = argv[3];
+    const char *inputimg_r = argv[1];
+    const char *inputimg_l = argv[2];
+    const char *outputimg = argv[3];
 
-    unsigned char* image_r = 0;
-    unsigned char* image_l = 0;
-    unsigned char* rzd_image_r = 0;
-    unsigned char* rzd_image_l = 0;
-    unsigned char* grayscale_r = 0;
-    unsigned char* grayscale_l = 0;
-    unsigned char* output = 0;
+    unsigned char *image_r = 0;
+    unsigned char *image_l = 0;
+    unsigned char *rzd_image_r = 0;
+    unsigned char *rzd_image_l = 0;
+    unsigned char *grayscale_r = 0;
+    unsigned char *grayscale_l = 0;
+    unsigned char *output = 0;
 
     unsigned width, height, resizedWidth, resizedHeight;
 
-    unsigned char* disparityLR 
-    unsigned char* disparityRL
+    unsigned char *disparityLR unsigned char *disparityRL
 
-    decodeImage(inputimg_r, &image_r, &width, &height);
+        decodeImage(inputimg_r, &image_r, &width, &height);
     decodeImage(inputimg_l, &image_l, &width, &height);
     resizeImage(image_r, &rzd_image_r, &width, &height, &resizedWidth, &resizedHeight);
     resizeImage(image_l, &rzd_image_l, &width, &height, &resizedWidth, &resizedHeight);
